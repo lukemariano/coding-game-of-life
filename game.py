b3e6b5c23466f23a81eb0d23a55d9cd004969264
Lucas Mariano <lucas.svmariano@gmail.com>
2022-12-22 16:20:10 -0300

    initial

---
+++ game.py
@@ -0,0 +1,16 @@
+import copy
+import random
+import sys
+import time
+
+# Set up the constants:
+WIDTH = 79   # The width of the cell grid.
+HEIGHT = 20  # The height of the cell grid.
+
+# (!) Try changing ALIVE to '#' or another character:
+ALIVE = 'O'  # The character representing a living cell.
+# (!) Try changing DEAD to '.' or another character:
+DEAD = ' '   # The character representing a dead cell.
+
+# (!) Try changing ALIVE to '|' and DEAD to '-'.
+
