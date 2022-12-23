8c1dd547460e185c229d79a448e5399ea237207d
Lucas Mariano <lucas.svmariano@gmail.com>
2022-12-23 16:16:25 -0300

    add cell, grid and controller components

---
+++ vuejs-game-version/src/components/GridBoard.vue
@@ -0,0 +1,265 @@
+<template>
+  <div>
+    <div
+      class="game-grid columns"
+      @mousedown="isMouseDown = true"
+      @mouseup="isMouseDown = false"
+      @mouseleave="isMouseDown = false"
+    >
+      <div v-for="(col, indexX) in gridList" :key="indexX" class="game-column">
+        <app-cell
+          v-for="(isAlive, indexY) in col"
+          :key="indexY"
+          :status-obj="isAlive"
+          :x-pos="indexX"
+          :y-pos="indexY"
+          :is-mouse-down="isMouseDown"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Cell from "@/components/CellGrid.vue";
+export default {
+  components: {
+    "app-cell": Cell,
+  },
+  props: {
+    message: {
+      default: "",
+      type: String,
+    },
+    importToken: {
+      default: "",
+      type: String,
+    },
+  },
+  data() {
+    return {
+      width: 40,
+      height: 23,
+      gridList: [],
+      // A prop that gets used by the app-cell component (drag)
+      isMouseDown: false,
+    };
+  },
+  computed: {},
+  watch: {
+    /**
+     * Watches for changes in the message prop
+     * that gets passed down from the App component
+     * and then handles the input on a specific tick.
+     *
+     * @param {string} val - the value
+     */
+    message: function (val) {
+      if (val === "nextStep") {
+        this.update();
+      } else if (val === "redoSession") {
+        this.reset();
+      } else if (val === "randomSeed") {
+        this.randomSeed();
+      } else if (val === "importSession") {
+        this.importSession();
+      } else if (val === "exportSession") {
+        this.exportSession();
+      }
+    },
+  },
+  created() {
+    this.cellCalc();
+  },
+  methods: {
+    /**
+     * Creates a 2D-Array during runtime for
+     * the website to use for most operations.
+     */
+    cellCalc: function () {
+      for (let i = 0; i < this.width; i++) {
+        this.gridList[i] = [];
+        for (let j = 0; j < this.height; j++) {
+          this.gridList[i][j] = { isAlive: false };
+        }
+      }
+    },
+    /**
+     * Changes the 'isAlive' object property
+     * of a specific cell to the one requested
+     * in the param.
+     *
+     * @param {number} x - the x position
+     * @param {number} y - the y position
+     * @param {boolean} bool - the new boolean
+     */
+    setCell: function (x, y, bool) {
+      if (this.gridList[x][y].isAlive != bool) {
+        this.gridList[x][y].isAlive = bool;
+      }
+      // let row = this.gridList[x];
+      // row.splice(y, 1, {isAlive: true});
+      // this.gridList.splice(x, 1, row);
+    },
+    /**
+     * The main function that updates the grid
+     * every tick based on the game of life rules.
+     */
+    update: function () {
+      let tempArr = [];
+      for (let i = 0; i < this.width; i++) {
+        tempArr[i] = [];
+        for (let j = 0; j < this.height; j++) {
+          let status = this.gridList[i][j].isAlive;
+          let neighbours = this.getNeighbours(i, j);
+          let result = false;
+          // Rule 1:
+          // Any live cell with fewer than two live neighbours dies,
+          // as if by under population
+          if (status && neighbours < 2) {
+            result = false;
+          }
+          // Rule 2:
+          // Any live cell with two or three neighbours lives on
+          // to the next generation
+          if ((status && neighbours == 2) || neighbours == 3) {
+            result = true;
+          }
+          // Rule 3:
+          // Any live cell with more than three live neighbours dies,
+          // as if by overpopulation
+          if (status && neighbours > 3) {
+            result = false;
+          }
+          // Rule 4:
+          // Any dead cell with exactly three live neighbours becomes
+          // a live cell, as if by reproduction
+          if (!status && neighbours == 3) {
+            result = true;
+          }
+          tempArr[i][j] = result;
+        }
+      }
+      // set new gridList content
+      for (let i = 0; i < this.width; i++) {
+        for (let j = 0; j < this.height; j++) {
+          this.setCell(i, j, tempArr[i][j]);
+        }
+      }
+    },
+    /**
+     * Returns the amount of neighbours for
+     * a specific cell on the grid.
+     *
+     * @param {number} posX - the x position
+     * @param {number} posY - the Y position
+     * @return {number} neighbours - amount of neighbours
+     */
+    getNeighbours: function (posX, posY) {
+      let neighbours = 0;
+      if (posX <= this.width && posY <= this.height) {
+        for (let offsetX = -1; offsetX < 2; offsetX++) {
+          for (let offsetY = -1; offsetY < 2; offsetY++) {
+            let newX = posX + offsetX;
+            let newY = posY + offsetY;
+            // check if offset is:
+            // on current cell, out of bounds and if isAlive
+            // for cell true
+            if (
+              (offsetX != 0 || offsetY != 0) &&
+              newX >= 0 &&
+              newX < this.width &&
+              newY >= 0 &&
+              newY < this.height &&
+              this.gridList[posX + offsetX][posY + offsetY].isAlive == true
+            ) {
+              neighbours++;
+            }
+          }
+        }
+      }
+      return neighbours;
+    },
+    /**
+     * Resets all gridList cells back to the
+     * start value.
+     */
+    reset: function () {
+      this.gridList.forEach((col) => {
+        col.forEach((cell) => {
+          cell.isAlive = false;
+        });
+      });
+    },
+    /**
+     * Populates and overwrites gridList with cells.
+     */
+    randomSeed: function () {
+      for (let i = 0; i < this.width; i++) {
+        for (let j = 0; j < this.height; j++) {
+          let rand = Math.random();
+          if (rand < 0.2) {
+            this.setCell(i, j, true);
+          } else {
+            this.setCell(i, j, false);
+          }
+        }
+      }
+    },
+    /**
+     * Resets and then imports new cells into the gridList
+     * based on the importToken prop that gets passed down
+     * App.vue.
+     * The importToken is a string and its syntax looks
+     * like this:
+     * '[xPos,yPos],[xPos,yPos]...'.
+     */
+    importSession: function () {
+      this.reset();
+      let regex = /\[\d+,\d+\]/gm;
+      let tempArr = this.importToken.match(regex);
+      if (tempArr) {
+        tempArr.forEach((element) => {
+          element = element.substring(1, element.length - 1);
+          let xy = element.split(",");
+          this.setCell(xy[0], xy[1], true);
+        });
+      }
+    },
+    /**
+     * Uses gridList to create an exportToken and
+     * emits it up to App.vue for the user to copy.
+     * Same format as in importToken().
+     */
+    exportSession: function () {
+      let exportToken = "";
+      for (let i = 0; i < this.width; i++) {
+        for (let j = 0; j < this.height; j++) {
+          if (this.gridList[i][j].isAlive) {
+            exportToken += "[" + i + "," + j + "]";
+          }
+        }
+      }
+      this.$emit("exportToken", exportToken);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.game-grid {
+  border-top: 1px solid #1a0707;
+  border-left: 1px solid #1a0707;
+  display: flex;
+  flex: 1;
+  justify-content: center;
+}
+.game-column {
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  padding: 0;
+  margin: 0 auto;
+  flex-direction: column;
+}
+</style>
--- vuejs-game-version/src/views/HomeView.vue
+++ vuejs-game-version/src/views/HomeView.vue
@@ -1,18 +1,36 @@
 <template>
   <div>
-    <NavBar />
+    <app-nav />
+    <app-grid />
+    <footer class="footer">
+      <div class="container">
+        <div class="columns">
+          <div class="column is-fullwidth">
+            <app-controller
+              :is-running="isRunning"
+              :main-component="mainComponent"
+              @send="delegate($event)"
+            />
+          </div>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
 <script>
 // import HelloWorld from "../components/HelloWorld";
 import NavBar from "../components/NavBar.vue";
+import GridBoard from "../components/GridBoard.vue";
+import ControllerGame from "@/components/ControllerGame.vue";
 
 export default {
   name: "HomeView",
 
   components: {
-    NavBar,
+    "app-nav": NavBar,
+    "app-grid": GridBoard,
+    "app-controller": ControllerGame,
   },
 };
 </script>
