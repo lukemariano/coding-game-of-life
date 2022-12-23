ef52275e3cd17684d0d50e33a6100237815cb1f1
Lucas Mariano <lucas.svmariano@gmail.com>
2022-12-23 19:45:12 -0300

    update config path

--- vuejs-game-version/vue.config.js
+++ vuejs-game-version/vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
-  publicPath: "/vuejs-game-version/",
+  publicPath: "/coding-game-of-life/",
 });
