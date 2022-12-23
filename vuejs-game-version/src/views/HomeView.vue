0b06b0effa29f5c5c796217531e544aa5c52503d
Lucas Mariano <lucas.svmariano@gmail.com>
2022-12-23 14:57:36 -0300

    initial restart vuejs gamelife version

---
+++ vuejs-game-version/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <NavBar />
+  </div>
+</template>
+
+<script>
+// import HelloWorld from "../components/HelloWorld";
+import NavBar from "../components/NavBar.vue";
+
+export default {
+  name: "HomeView",
+
+  components: {
+    NavBar,
+  },
+};
+</script>
